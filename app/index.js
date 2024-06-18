af00ae51bcfdf69e7fd194b8517027ab8171d657
fruitloaf <dmitrisanzharov@gmail.com>
2024-06-18 20:34:08 +0100

    all good

--- app/index.js
+++ app/index.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { Stack } from "expo-router";
 
 const Home = () => {
 	return (
 		<View>
-			<Text>Hello</Text>
+			<Stack.Screen
+				options={{
+					title: 'Home',
+					headerStyle: {
+						backgroundColor: "#f4511e",
+					},
+					headerTitleAlign: 'center',
+					headerTintColor: "#fff", // doesn’t seem to do anything
+					headerTitleStyle: {
+						fontWeight: "bold",
+						color: 'yellow', // <--- HERE COLOR CHANGE
+					},
+					headerShadowVisible: false,
+					headerLeft: () => <Text>Left</Text>,
+					headerRight: () => <Text>Right</Text>
+				}}
+			/>
+			<Text>Start</Text>
 		</View>
 	);
 };
