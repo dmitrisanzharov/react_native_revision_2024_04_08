93abf3b0f33d6dd2c718292f3103c1961b71b9ce
fruitloaf <dmitrisanzharov@gmail.com>
2024-06-19 20:31:00 +0100

    all good

--- app/index.js
+++ app/index.js
@@ -1,28 +1,76 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import {
+	View,
+	Text,
+	ScrollView,
+	TextInput,
+	TouchableOpacity,
+	Image,
+	ActivityIndicator,
+	FlatList,
+	Button 
+} from "react-native";
 import { Stack } from "expo-router";
 
 const Home = () => {
+	const [text, setText] = React.useState("");
+
+	function handleOnPress1() {
+		console.log("blah");
+	}
+
 	return (
 		<View>
 			<Stack.Screen
 				options={{
-					title: 'Home',
+					title: "Home",
 					headerStyle: {
 						backgroundColor: "#f4511e",
 					},
-					headerTitleAlign: 'center',
+					headerTitleAlign: "center",
 					headerTintColor: "#fff", // doesn’t seem to do anything
 					headerTitleStyle: {
 						fontWeight: "bold",
-						color: 'yellow', // <--- HERE COLOR CHANGE
+						color: "yellow", // <--- HERE COLOR CHANGE
 					},
 					headerShadowVisible: false,
 					headerLeft: () => <Text>Left</Text>,
-					headerRight: () => <Text>Right</Text>
+					headerRight: () => <Text>Right</Text>,
 				}}
 			/>
 			<Text>Start</Text>
+			<TextInput value={text} onChangeText={setText} placeholder="test" />
+			<TouchableOpacity
+				onPress={handleOnPress1}
+				style={{ borderWidth: 1 }}
+			>
+				<Text>Blah</Text>
+			</TouchableOpacity>
+			<Image
+				source={{ uri: "https://reactnative.dev/img/tiny_logo.pngg" }}
+				style={{ width: 25, height: 25 }}
+				resizeMode="cover"
+			/>
+			<ActivityIndicator size="large" color="violet" />
+			{/* <ScrollView></ScrollView> */}
+			<FlatList
+			style={{borderWidth: 1, height: 50}}
+				data={[
+					"fullTime",
+					"partTime",
+					"other",
+					"fullTime",
+					"partTime",
+					"other",
+					"fullTime",
+					"partTime",
+					"other",
+				]}
+				renderItem={(info) => <Text>{info.item}</Text>}
+				contentContainerStyle={{rowGap: 10}}
+				keyExtractor={item => item + String(Math.random())}
+			/>
+			<Button title="Press me" onPress={() => console.log("blah")} color='green' />
 		</View>
 	);
 };
